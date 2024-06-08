28f81f505dafe93616eb0b9d271e2300554c2512
Maou Arfin <156934678+arfin03@users.noreply.github.com>
2024-06-08 08:16:37 +0700

    Update queue.py

--- AnonXMusic/utils/inline/queue.py
+++ AnonXMusic/utils/inline/queue.py
@@ -71,6 +71,7 @@ def aq_markup(_, chat_id):
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
-        [InlineKeyboardButton(text=_["𝙶𝚛𝚘𝚞𝚙𝚜"], url=f'https://t.me/gc_animecommunity')],
+        [InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+        ],
     ]
     return buttons
